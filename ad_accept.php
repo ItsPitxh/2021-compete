397a4824997dff9bd27fbd231690d2e4f347561c
ItsPitxh <pitched.10121@gmail.com>
2021-12-09 13:06:55 +0700

    admin php

---
+++ ad_accept.php
@@ -0,0 +1,46 @@
+<?php
+include("ad_nav.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>#</title>
+</head>
+<body>
+<div class="main">
+        <form action="" method="post">
+            <input type="search" name="search" placeholder="ค้นหาชื่อผู้ใช้ . . ." class="form-control">
+        </form>
+
+        <div class="table-responsive mt-5">
+            <table class="table">
+              <thead>
+                <tr>
+                  <th scope="col">ชื่อจริง</th>
+                  <th scope="col">นามสกุล</th>
+                  <th scope="col">ชื่อผู้ใช้</th>
+                  <th scope="col">โปรไฟล์</th>
+                  <th scope="col">สถานะ</th>
+                  <th scope="col">อนุญาติ</th>
+                  <th scope="col">ไม่อนุญาติ</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>ปรเมษ</td>
+                  <td>เวชสนิท</td>
+                  <td>por</td>
+                  <td><img src="" style="width: 30%;"></td>
+                  <td>มีแฟนแล้ว</td>
+                  <td><a href="#" class="btn btn-success">อนุญาติ</a></td>
+                  <td><a href="#" class="btn btn-danger">ไม่อนุญาติ</a></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+    </div>
+</body>
+</html>
---
+++ ad_nav.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administer | Lannapoly</title>
+    <!-- css -->
+    <link rel="stylesheet" href="style/style/ad.css">
+    <link rel="stylesheet" href="style/css/bootstrap.css">
+</head>
+
+<body>
+    <div class="fixed-top">
+        <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #1a1a1a">
+            <a class="navbar-brand" href="#">#</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" id="accept">Accept</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" id="ee">#</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#" id="cc">#</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn btn-danger" href="" id="ll">Logout</a>
+                    </li>
+                </ul>
+            </div>
+            <a href="#" id="logout" class="btn btn-danger">Logout</a>
+        </nav>
+    </div>
+
+    <div class="sidenav">
+        <a href="#">Accept</a>
+        <hr color="grey">
+        <a href="#">#</a>
+        <hr color="grey">
+        <a href="#">#</a>
+        <hr color="grey">
+    </div>
+
+    <!-- js -->
+    <script src="style/js/jquery-3.6.0.min.js"></script>
+    <script src="style/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
